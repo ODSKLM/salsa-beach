21f2ab7303783c6b3266aa824b931115efadf064
Daan Debie <daan.debie@klm.com>
2020-07-09 18:19:35 +0200

    fix(types): replace Single Abstract Methods with regular anonymous class instantiations to enable Scala 2.11 support

--- types/src/main/scala/com/odsklm/salsabeach/types/rowconverters/RowKeyDecoder.scala
+++ types/src/main/scala/com/odsklm/salsabeach/types/rowconverters/RowKeyDecoder.scala
@@ -1,11 +1,17 @@
 package com.odsklm.salsabeach.types.rowconverters
 
+/**
+  * RowKeyDecoder is a type class for decoding row keys, represented as byte arrays, into concrete types
+  * @tparam A type to decode the row keys to
+  */
 trait RowKeyDecoder[A] {
-  def decode(rk: Array[Byte]): A
+  def decode(rowKey: Array[Byte]): A
 }
 
 object RowKeyDecoder {
-  def apply[A](decode: Array[Byte] => A): RowKeyDecoder[A] = (rk: Array[Byte]) => decode(rk)
+  def apply[A](decodeFn: Array[Byte] => A): RowKeyDecoder[A] = new RowKeyDecoder[A] {
+    override def decode(rowKey: Array[Byte]): A = decodeFn(rowKey)
+  }
 
   object ops {
     def decode[A: RowKeyDecoder](rk: Array[Byte]): A =
--- types/src/main/scala/com/odsklm/salsabeach/types/rowconverters/RowQueryEncoder.scala
+++ types/src/main/scala/com/odsklm/salsabeach/types/rowconverters/RowQueryEncoder.scala
@@ -3,14 +3,16 @@ package com.odsklm.salsabeach.types.rowconverters
 /**
   * RowQueryEncoder is a type class that lets you translate any type into a byte string that can be used to find one
   * or more rows in HBase. It is used for all HBase operations: get, put, scan, delete
-  * @tparam A
+  * @tparam A type to encode
   */
 trait RowQueryEncoder[A] {
   def encode(instance: A): Array[Byte]
 }
 
 object RowQueryEncoder {
-  def apply[A](encode: A => Array[Byte]): RowQueryEncoder[A] = (instance: A) => encode(instance)
+  def apply[A](encodeFn: A => Array[Byte]): RowQueryEncoder[A] = new RowQueryEncoder[A] {
+    override def encode(instance: A): Array[Byte] = encodeFn(instance)
+  }
 
   object instances {
     implicit val utf8StringRowKeyEncoder: RowQueryEncoder[String] =
